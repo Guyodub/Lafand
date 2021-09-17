e6689f8bc33c0c9d037cccf760cc773d7a7a88e1
Freshia <freshiasackey@gmail.com>
2021-09-17 10:18:10 +0300

    Added web scraper

---
+++ G Voices Web Scraper/scraper.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+en_url= "https://globalvoices.org/2021/05/28/trinidad-and-tobago-edges-closer-to-amending-its-equal-opportunity-act-to-include-sexual-orientation/"
+sw_url = "https://sw.globalvoices.org/2021/05/trinidad-na-tobago-yakaribia-kurekebisha-sheria-ya-fursa-sawa-kutambua-ushoga/"
+
+sw_file = open("swahili.sw","a")
+en_file = open("english.en","a")
+
+def scrap_text(url):
+    r1 = requests.get(url)
+    mypage = r1.content
+
+    soup1 = BeautifulSoup(mypage, "html.parser")
+    article = soup1.find_all('div', class_='entry full-post-content')
+
+    txt=article[0].get_text()
+    print(txt)
+    return txt
+
+def write_to_file(sw_text,en_text):
+    sw_file.write(sw_text)
+    en_file.write(en_text)
+
+sw_text = scrap_text(sw_url)
+en_text = scrap_text(en_url)
+
+write_to_file(sw_text,en_text)
