67c57582cd63a3ab7726a5bb3c131e3fbeb02a62
Freshia <freshiasackey@gmail.com>
2021-09-18 16:55:45 +0300

    Support to read from spreadsheet

--- G Voices Web Scraper/scraper.py
+++ G Voices Web Scraper/scraper.py
@@ -1,10 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
-en_url= "https://globalvoices.org/2021/05/28/trinidad-and-tobago-edges-closer-to-amending-its-equal-opportunity-act-to-include-sexual-orientation/"
-sw_url = "https://sw.globalvoices.org/2021/05/trinidad-na-tobago-yakaribia-kurekebisha-sheria-ya-fursa-sawa-kutambua-ushoga/"
+import xlrd
 
-sw_file = open("swahili.sw","a")
-en_file = open("english.en","a")
+
+# en_url= "https://globalvoices.org/2021/05/28/trinidad-and-tobago-edges-closer-to-amending-its-equal-opportunity-act-to-include-sexual-orientation/"
+# sw_url = "https://sw.globalvoices.org/2021/05/trinidad-na-tobago-yakaribia-kurekebisha-sheria-ya-fursa-sawa-kutambua-ushoga/"
+
+
+excel_file = "../Corpus_links.xlsx"
+
+sw_file = open("swahili.sw","a", encoding = "utf-8")
+en_file = open("english.en","a", encoding = "utf-8")
 
 def scrap_text(url):
     r1 = requests.get(url)
@@ -21,7 +27,17 @@ def write_to_file(sw_text,en_text):
     sw_file.write(sw_text)
     en_file.write(en_text)
 
-sw_text = scrap_text(sw_url)
-en_text = scrap_text(en_url)
+def read_from_spreadsheet(excel_file):
+    workbook = xlrd.open_workbook(excel_file)
+    sheet = workbook.sheet_by_index(0)
 
-write_to_file(sw_text,en_text)
+    for i in range(1,sheet.nrows):
+        sw_url = sheet.cell_value(i, 0)
+        en_url = sheet.cell_value(i, 1)
+
+        sw_text = scrap_text(sw_url)
+        en_text = scrap_text(en_url)
+
+        write_to_file(sw_text,en_text)
+
+read_from_spreadsheet(excel_file)
